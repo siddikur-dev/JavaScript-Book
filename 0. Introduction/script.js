4182182905cad0613f526430f3c124adbb993ebf
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-10 02:50:59 +0600

    javascript file has been added here

---
+++ 0. Introduction/script.js
@@ -0,0 +1,31 @@
+// Variable 
+var elements = 'Oxygen';
+let person = 'A Great Man';
+const number = 200;
+
+// Condition
+let age = 22;
+if (age >= 20) {
+    console.log('I will Get married');
+}
+else {
+    console.log('You Are Not Adult');
+}
+
+// Array 
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Loop 
+for (let day = 1; day <= 60; day++) {
+    console.log('I will invest at least 6 hrs every single day for next 60 days!', day);
+}
+
+// Object 
+const mobile = {
+    brand: 'Samsung',
+    name: 'S20',
+    color: 'Blue',
+    isNew: true,
+    price: '50K'
+}
